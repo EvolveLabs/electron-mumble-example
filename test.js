1eec3f6a6d7973821706b136522b35fb79a9f21e
Justin Chase <justin@evovlehq.com>
2015-06-25 15:58:59 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,62 @@
+var mumble = require('electron-mumble')
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var queue = []
+    var playing = false
+    var client = null
+    var startTime = 0
+    var actx = new AudioContext()
+
+    mumble.connect( 'voice.evolvehq.com', {}, function ( error, _client ) {
+        if( error ) { throw new Error( error ); }
+
+        console.log( 'Connected' );
+
+        client = _client
+        client.authenticate( 'mrbumpy', 'gameplantest' );
+        client.on( 'initialized', onInit );
+        client.on( 'voice', onVoice );
+    });
+
+    var onInit = function() {
+        console.log( 'Connection initialized' );
+    };
+
+    var onVoice = function( pcmData ) {
+        var size = pcmData.length / 2
+        var data = new Float32Array(size)
+        for(var i = 0; i < size; i++) {
+            data[i] = pcmData.readInt16LE( i * 2 ) / 32768
+        }
+
+        if (playing || queue.length > 0) {
+            queue.push(data);
+        } else {
+            play(data)
+        }
+    }
+
+    var playNext = function () {
+        if(queue.length > 0) {
+            var data = queue.shift()
+            play(data)
+        } else {
+            playing = false
+        }        
+    }
+
+    var play = function (data) {
+        playing = true
+
+        var buffer = actx.createBuffer(1, data.length, client.connection.SAMPLING_RATE)
+        buffer.getChannelData(0).set(data)
+
+        var source = actx.createBufferSource()
+        source.connect(actx.destination)
+        source.onended = playNext
+        source.buffer = buffer
+        source.start(startTime)
+        startTime += buffer.duration
+    }
+})
