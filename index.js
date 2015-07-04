7a766f3d30e0a432a241e4003750a2a9312b9f4d
justinmchase <justin.m.chase@gmail.com>
2015-07-04 15:04:43 -0500

    Angularize the demo. Send mic data to server.
    There is a problem with sending audio to the server still. It appears to be in node-mumble itself.
    Perhaps the opus encoder isn't working on windows. I'm still investigating.

--- index.js
+++ index.js
@@ -1,73 +1,18 @@
-var mumble = require('electron-mumble')
 
-var connect;
+var app = angular
+    .module('main', ['ui.router', 'ui.bootstrap'])
+    .service('mumbleService', require('./services/mumble'))
+    .service('storageService', require('./services/storage'))
+    .controller('NavigationController', require('./controllers/navigation'))
+    .controller('ConnectionController', require('./controllers/connection'))
+    .controller('BasicController', require('./controllers/basic'))
+    .controller('AdvancedController', require('./controllers/advanced'))
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    var queue = []
-    var playing = false
-    var client = null
-    var startTime = 0
-    var actx = new AudioContext()
-
-    connect = function () {
-
-    var server = document.getElementById('server').value
-    var username = document.getElementById('username').value
-    var password = document.getElementById('password').value
-
-    console.log('Connecting to: ' + server + '...')
-
-    mumble.connect( server, {}, function ( error, _client ) {
-        if( error ) { throw new Error( error ); }
-
-        console.log( 'Connected' );
-
-        client = _client
-        client.authenticate( username, password );
-        client.on( 'initialized', onInit );
-        client.on( 'voice', onVoice );
-    });
-
-    var onInit = function() {
-        console.log( 'Connection initialized' );
-    };
-
-    var onVoice = function( pcmData ) {
-        var size = pcmData.length / 2
-        var data = new Float32Array(size)
-        for(var i = 0; i < size; i++) {
-            data[i] = pcmData.readInt16LE( i * 2 ) / 32768
-        }
-
-        if (playing || queue.length > 0) {
-            queue.push(data);
-        } else {
-            play(data)
-        }
-    }
-
-    var playNext = function () {
-        if(queue.length > 0) {
-            var data = queue.shift()
-            play(data)
-        } else {
-            playing = false
-        }        
-    }
-
-    var play = function (data) {
-        playing = true
-
-        var buffer = actx.createBuffer(1, data.length, client.connection.SAMPLING_RATE)
-        buffer.getChannelData(0).set(data)
-
-        var source = actx.createBufferSource()
-        source.connect(actx.destination)
-        source.onended = playNext
-        source.buffer = buffer
-        source.start(startTime)
-        startTime += buffer.duration
-    }
-    }
-})
+        $urlRouterProvider.otherwise('main/connection')
+        $stateProvider
+            .state('main', { abstract: true, url: '/main', templateUrl: 'templates/main.html' })
+            .state('main.connection', { url: '/connection', templateUrl: 'templates/connection.html' })
+            .state('main.basic', { url: '/basic', templateUrl: 'templates/basic.html' })
+            .state('main.advanced', { url: '/advanced', templateUrl: 'templates/advanced.html' })
+    })
